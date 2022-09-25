938e11c15e61e9f0bc8963831170834cf131e287
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2022-09-25 18:44:01 -0400

    Time: 5 ms (24.03%), Space: 6.3 MB (72.46%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> s_stack;
-        stack<char> t_stack;
         
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '#'&& s_stack.empty() == false) {
-                s_stack.pop();
-            }
-            else{
-                if(s[i] != '#') {
-                    s_stack.push(s[i]);
+        int i = 0;
+        while(i < s.size()) {  
+            if(s[i] == '#') {
+                if(i - 1 >= 0) {
+                    s.erase(i - 1, 2);
+                    i = i - 2;
+                }
+                else{
+                    s.erase(i, 1);
+                    i = i -1;
                 }
             }
+            i++;
         }
-        
-        for(int i = 0; i < t.size(); i++) {
-            if(t[i] == '#' && t_stack.empty() == false) {
-                t_stack.pop();
-            }
-            else{
-                if(t[i] != '#') {
-                    t_stack.push(t[i]);
+        i = 0;
+        while(i < t.size()) {
+            if(t[i] == '#') {
+                if(i - 1 >= 0) {
+                    t.erase(i - 1, 2);
+                    i = i - 2;
+                }
+                else{
+                    t.erase(i, 1);
+                    i = i - 1;
                 }
             }
+            i++;
         }
-       
-        stack<char> s_stack_copy = s_stack;
-        stack<char> t_stack_copy = t_stack;
-        
-        while(s_stack_copy.empty() == false) {cout << s_stack_copy.top();
-                                        s_stack_copy.pop();}
-        std::cout << endl;
-        while(t_stack_copy.empty() == false) {cout << t_stack_copy.top();
-                                        t_stack_copy.pop();}
-        if(s_stack.size() != t_stack.size()) {return false;}
-        while(t_stack.empty() == false) {
-            cout << "s: " << s_stack.top() << " " << endl;
-            cout << "t: " << t_stack.top() << " " << endl;
-            
-            if(s_stack.top() != t_stack.top()) {
-                return false;
-            }
-            else {
-                s_stack.pop();
-                t_stack.pop();
-            }
+        cout << s << " ";
+        cout << t;
+        if(s == t) {
+            return true;
         }
-        return true;
+        return false;
     }
 };
