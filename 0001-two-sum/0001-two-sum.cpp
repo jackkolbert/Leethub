be9fce95b1cc3515fca2ada588043f3d0b7bd10f
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2022-10-24 22:48:43 -0400

    Time: 38 ms (48.89%), Space: 12.2 MB (8.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
-        vector<int> ret_vec;
+        vector<int> ret_vec(2);
         for(int i = 0; i < nums.size(); i++) {
             //store the indices in the value - key is the thing we 
             int temp = target - nums[i];
@@ -11,12 +11,11 @@ public:
         
         for(int k = 0; k < nums.size(); k++) {
             if(map.find(nums[k]) != map.end() && map[nums[k]] != k) {
-                ret_vec.push_back(map[nums[k]]);
-                ret_vec.push_back(k);
+                ret_vec[0] = map[nums[k]];
+                ret_vec[1] = k;
                 return ret_vec;
             }
         }
-        
         return ret_vec;
     }
 };
