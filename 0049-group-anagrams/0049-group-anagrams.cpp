d5abfcb577f3046acd8d57b999f53cef64a41f59
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2022-10-30 18:15:13 -0400

    Time: 93 ms (25.19%), Space: 26.4 MB (10.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -8,11 +8,9 @@ public:
             vector<int>temp_vec(26, 0);
             
             for(int k = 0; k < strs[i].size(); k++) {
-                //counting letters
                 string temp_s = strs[i];
                 int temp_i = temp_s[k];
                 temp_i = temp_i - '0' - 49;
-                //cout << temp_i << " ";
                 temp_vec[temp_i] += 1;
             }
             maps[temp_vec].push_back(strs[i]);
