178204ab83ed6a6328193f86b0b758501dcba2c8
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-02-27 21:44:23 -0500

    Time: 35 ms (8.85%), Space: 5.9 MB (62.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string eric = to_string(x);
+        //cout << eric;
+        int p = eric.size()-1;
+        
+        for(int i = 0; i < eric.size() / 2 + 1; i++) {
+            if(eric[i] == eric[p]) {
+                p--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
