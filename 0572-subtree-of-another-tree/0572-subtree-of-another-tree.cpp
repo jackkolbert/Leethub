fcacd56f6c0b91b130035526737da2ebef1cb293
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-03-02 00:24:25 -0500

    Time: 32 ms (35.94%), Space: 28.8 MB (85.32%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+                
+
+        return helper_1(root, subRoot);
+        
+    }
+    bool helper_1(TreeNode* root, TreeNode* subRoot) {
+        if(root == nullptr && subRoot == nullptr) {
+            return true;
+        }
+        else if(root != nullptr && subRoot == nullptr) {
+            return false;
+        }
+        else if(root == nullptr && subRoot != nullptr) {
+            return false;
+        }
+        if(root->val == subRoot->val) {
+            bool temp = isSame(root->left, subRoot->left) 
+                        && isSame(root->right, subRoot->right); 
+            if (temp) {
+                return temp;
+            }
+        }
+        
+            return helper_1(root->left, subRoot) || helper_1(root->right, subRoot);    
+        
+    }
+    
+    bool isSame(TreeNode* root, TreeNode* subRoot) {
+        
+        if(root == nullptr && subRoot == nullptr) {
+            return true;
+        }
+        else if(root != nullptr && subRoot == nullptr) {
+            return false;
+        }
+        else if(root == nullptr && subRoot != nullptr) {
+            return false;
+        }
+        else if(subRoot->val == root->val) {
+            return isSame(root->left, subRoot->left) 
+                && isSame(root->right, subRoot->right);
+        }
+        return false;
+    }
+};
