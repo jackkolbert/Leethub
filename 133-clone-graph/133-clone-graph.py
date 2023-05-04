b751fb52a2fc10e8c3acf369b4b6d7b38fc6ea04
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-04 18:35:01 -0400

    Time: 49 ms (8.16%), Space: 16.8 MB (5.72%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -1,31 +1,15 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        ret_dict = {}                
-        if node is None:
-            return None
-        return self.clone(node, ret_dict)
-        
-    def clone(self, node, ret_dict):
-        
-        # check if node taken care of already
-        if node in ret_dict:
-            return ret_dict[node]
-        
-        else:
+    def cloneGraph(self, node: "Node") -> "Node":
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
             copy = Node(node.val)
-            ret_dict[node] = copy
-
+            oldToNew[node] = copy
             for nei in node.neighbors:
-                copy.neighbors.append(self.clone(nei, ret_dict))
-        
-            return copy 
+                copy.neighbors.append(dfs(nei))
+            return copy
 
-        
+        return dfs(node) if node else None
