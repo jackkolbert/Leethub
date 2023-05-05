ea6654beb0827ec266c5fdab42a626d121c43195
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-05 00:35:54 -0400

    Time: 143 ms (8.90%), Space: 19.3 MB (6.88%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        self.finishable = set()
+        # form graph from preqs
+        self.c_preq = {i: [] for i in range(0, numCourses)} # from course returns preqs
+        for preq in prerequisites:
+            self.c_preq[preq[1]].append(preq[0])
+
+        # all courses
+        for i in range(0, numCourses):
+            status = self.preq_check(i, [])
+            if status is False:
+                return False
+        return True
+                        
+    def preq_check(self, course, visited):
+        
+        if len(self.c_preq[course]) == 0 or course in self.finishable:
+            return True
+        
+        else:
+            if course in visited:
+                return False
+            temp = visited
+            temp.append(course)
+            
+            for preq in self.c_preq[course]:
+                status = self.preq_check(preq, temp)
+                if status is False:
+                    return False
+                else:
+                    self.finishable.add(preq)
+        return True
+        
+            
