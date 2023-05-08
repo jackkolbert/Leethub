e5220b571ab724d2b0f156cdc74e46fe859fb7e6
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-08 15:03:47 -0400

    Time: 98 ms (45.15%), Space: 23.2 MB (5.65%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,18 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         
-        memo = [0] * (n + 1)
-        memo[0] = 0
-        if n > 0:
-            memo[1] = 1
-        if n > 1:
-            memo[2] = 1
-        if n > 2:
-            memo[3] = 2
-        a = 4
-        for i in range(4, n + 1):
-            if a * 2 == i:
-                a = i
-            memo[i] = 1 + memo[i-a]
-            
-        return memo
+        dp = [None] * (n+1)
+        
+        dp[0] = 0
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i-offset]
+        return dp
