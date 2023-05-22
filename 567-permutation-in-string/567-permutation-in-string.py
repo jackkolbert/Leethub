93376061bf014a878bd4bac682f07dd241a1944e
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-21 23:09:06 -0400

    Time: 331 ms (26.01%), Space: 16.6 MB (5.87%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -0,0 +1,50 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+        
+        s1_map = {}
+        for i in s1:
+            if i not in s1_map:
+                s1_map[i] = 1
+            else:
+                s1_map[i] += 1
+        left = 0
+        right = len(s1)
+        
+        window = {}
+        
+        for i in range(0, right):
+            if s2[i] in window:
+                window[s2[i]] += 1
+            else:
+                window[s2[i]] = 1
+        right -= 1
+        while(True):
+            
+            if s1_map == window:
+                return True
+            print(s1_map)
+            print(window)
+            window[s2[left]] -= 1
+            if window[s2[left]] == 0:
+                window.pop(s2[left])
+            left += 1
+
+            right +=  1
+            
+            if right == len(s2):
+                break
+                
+            if s2[right] in window:
+                window[s2[right]] += 1
+            else:
+                window[s2[right]] = 1
+            
+        return False
+                
+        
+        
+                
+            
