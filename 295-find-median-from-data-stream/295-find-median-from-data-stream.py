a808e9baac178a0f0e6b208dddc4b4811187087b
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-22 23:24:17 -0400

    Time: 501 ms (84.71%), Space: 38.5 MB (13.53%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,19 +1,47 @@
 class MedianFinder:
 
     def __init__(self):
-        self.queue = []
+        self.minHeap = []
+        self.maxHeap = []
 
     def addNum(self, num: int) -> None:
-        self.queue.append(num)
 
-    def findMedian(self) -> float:      
-        self.queue = sorted(self.queue)
-        #print(self.queue)
-        s = len(self.queue)
-        if s % 2 == 0:
-            return (self.queue[s // 2] + self.queue[(s // 2) - 1]) / 2
+        
+        if len(self.minHeap) == 0:
+            self.minHeap.append(num)
+            return
+        
+        if num < self.minHeap[0]:
+            heapq.heappush(self.maxHeap, num * -1)
         else:
-            return self.queue[s // 2]
+            heapq.heappush(self.minHeap, num)
+
+        
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            temp = heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, temp * -1)
+
+        elif len(self.maxHeap) > len(self.minHeap) + 1:
+            temp = heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, temp * -1)
+
+    def findMedian(self) -> float:
+        if len(self.minHeap) == 0:
+            return self.maxHeap[0] * - 1
+        if len(self.maxHeap) == 0:
+            return self.minHeap[0]
+        
+        
+        if (len(self.minHeap) + len(self.maxHeap)) % 2 == 0:
+           
+            return (self.minHeap[0] + (-1 * self.maxHeap[0])) / 2
+
+        else:
+            if len(self.minHeap) > len(self.maxHeap):
+                return self.minHeap[0]
+            else:
+                return -1 * self.maxHeap[0]
+
 
 
 # Your MedianFinder object will be instantiated and called as such:
