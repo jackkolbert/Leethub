7a53fdff843ca41e86076a176aec92ab51617701
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-27 20:12:42 -0400

    Time: 48 ms (30.10%), Space: 16.3 MB (14.27%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        self.memo = {}
+        
+        return self.dfs(nums, 0, 0)
+        
+    def dfs(self, nums, curr_ind, curr_money):
+        
+        if curr_ind >= len(nums):
+            return 0
+        
+        elif curr_ind in self.memo:
+            return self.memo[curr_ind]
+        
+        elif curr_ind == len(nums) - 1:
+            return nums[len(nums)-1]
+        
+        a = self.dfs(nums, curr_ind+1, curr_money)
+        b = self.dfs(nums, curr_ind+2, curr_money+nums[curr_ind]) + nums[curr_ind]
+        
+        self.memo[curr_ind] = max(a, b)
+        return self.memo[curr_ind]
+        
