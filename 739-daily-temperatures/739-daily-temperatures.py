33efbaa6580558dc8e13bcf929e33703aa427336
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-05-29 16:46:39 -0400

    Time: 1435 ms (15.99%), Space: 31.4 MB (19.09%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -4,7 +4,7 @@ class Solution:
         stack = []  # holds temp, index
         stack.append((temperatures[0], 0))
         
-        ret = [None] * (len(temperatures))
+        ret = [0] * (len(temperatures))
         
         for i in range(1, len(temperatures)):
             while len(stack) > 0 and temperatures[i] > stack[-1][0]:
@@ -12,8 +12,5 @@ class Solution:
                 ret[ind] = i - ind
                 stack.pop()
             stack.append((temperatures[i], i))
-        
-        for i in range(len(stack)):
-            ind = stack[i][1]
-            ret[ind] = 0
+
         return ret
