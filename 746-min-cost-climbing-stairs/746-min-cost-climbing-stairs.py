ad0032a733f45c7ff77242cc0a4a1140327da2d8
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-06-05 23:22:34 -0400

    Time: 91 ms (5.33%), Space: 19 MB (6.21%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        memo = {}
+        def rec(i):
+            if i >= len(cost):
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            else:
+                a = min(rec(i+1), rec(i+2)) + cost[i]
+                memo[i] = a
+                return memo[i]
+        
+        return min(rec(1), rec(0))
