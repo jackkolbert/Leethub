6c048fcc63f6f06b9636b1b17fe61cd24d71677b
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-06-18 20:13:18 -0400

    Time: 1520 ms (32.45%), Space: 46.6 MB (7.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,33 +2,30 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
         memo = {}
-        memo[0] = 0
-        def dfs(amount):
-            
-            if amount in memo:
-                return memo[amount]
         
-            if amount < 0:
+        def dfs(curr):
+            
+            if curr in memo:
+                return memo[curr]
+            elif curr == 0:
+                return 0
+            elif curr < 0:
                 return -1
-        
-            g_min = sys.maxsize
-            found = False
-            
-            for coin in coins:
-                temp = dfs(amount - coin)
-                if temp != -1:
-                    g_min = min(g_min, temp)
-                    found = True
-            if found == False:
-                memo[amount] = -1
-                return -1
-            memo[amount] = g_min + 1
-            return memo[amount]
-            
-        
-        for i in range(1, amount + 1):
-            dfs(i)
-
-        return memo[amount]
-            
+            else:
+                min_coins = sys.maxsize
+                for coin in coins:
+                    temp = dfs(curr - coin)
+                    if temp == -1:
+                        continue
+                    else:
+                        min_coins = min(temp+1, min_coins)
+                if min_coins == sys.maxsize:
+                    memo[curr] = -1
+                    return -1
+                else:
+                    memo[curr] = min_coins
+                    return min_coins
+        return dfs(amount)
                 
+                        
+                        
