934577cb18fa827d52ad01bb99689b38133a96b8
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-07-09 00:19:11 -0400

    Time: 103 ms (16.56%), Space: 30.2 MB (23.33%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        
+        dupe = {}
+        ret = []
+        l = 0
+        r = 10
+        
+        while r <= len(s):
+            wind = s[l:r]
+            print(wind)
+            if wind in dupe:
+                if dupe[wind] == 1:
+                    print('hi')
+                    ret.append(wind)
+                dupe[wind] += 1
+            
+            else:
+                dupe[wind] = 1
+                
+            l += 1
+            r += 1
+        return ret
+            
