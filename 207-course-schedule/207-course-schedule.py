94510aabed7a0dcb8417157afa33c771592debfb
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-07-12 21:29:25 -0400

    Time: 119 ms (40.54%), Space: 19.3 MB (24.42%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,35 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        self.finishable = set()
-        # form graph from preqs
-        self.c_preq = {i: [] for i in range(0, numCourses)} # from course returns preqs
-        for preq in prerequisites:
-            self.c_preq[preq[1]].append(preq[0])
+        preq = prerequisites
+        adj = {}
 
-        # all courses
-        for i in range(0, numCourses):
-            visited = set()
-            status = self.preq_check(i, visited)
-            if status is False:
-                return False
-        return True
-                        
-    def preq_check(self, course, visited):
-        
-        if len(self.c_preq[course]) == 0 or course in self.finishable:
-            return True
-        
-        else:
+        for i in range(numCourses):
+            adj[i] = []
+        for i in range(len(preq)):
+            adj[preq[i][0]].append((preq[i][1]))
+            
+        def dfs(course, visited):
+            if len(adj[course]) == 0:
+                return True
+            
             if course in visited:
                 return False
             visited.add(course)
             
-            for preq in self.c_preq[course]:
-                status = self.preq_check(preq, visited)
-                if status is False:
+            for i in range(len(adj[course])):
+                if dfs(adj[course][i], visited) is False:
                     return False
-                else:
-                    self.finishable.add(preq)
+            adj[course] = []
+            return True
+            
+            
+            
+        for i in range(numCourses):
+            if dfs(i, set()) is False:
+                return False
+           
         return True
-        
             
