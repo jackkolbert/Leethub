3a317e525ea044e7b0413a6b035b04ce54ab4a1c
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-07-15 15:40:13 -0400

    Time: 1170 ms (40.94%), Space: 63.8 MB (83.87%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -1,31 +1,26 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        pq = []
-    
         events.sort()
-        endDay = 0
-        for event in events:
-            endDay = max(endDay, event[1])
-            
-        startDay = events[0][0]
+        total_days = max(end for start, end in events)
+        day = 0
+        event_id = 0
+        num_events_attended = 0
+        min_heap = []
         
-        count = 0
-        ending_ind = 0
-        for i in range(startDay, endDay+1):
+        for day in range(1, total_days+1):
+            # Add all the events that start today
+            while event_id < len(events) and events[event_id][0] == day:
+                heappush(min_heap, events[event_id][1])
+                event_id += 1
             
-
-            while ending_ind < len(events) and i == events[ending_ind][0]:
-                heapq.heappush(pq, events[ending_ind][1])
-                ending_ind += 1
-
+            # Remove all the events whose end date was before today
+            while min_heap and min_heap[0] < day:
+                heappop(min_heap)
             
-            while len(pq) != 0 and pq[0] < i:
-                heapq.heappop(pq)
+            # if any event that can be attended today, let's attend it
             
-            if len(pq) != 0:           
-                a = heapq.heappop(pq)
-                print(a)
-                count += 1
-        return count
-        
-            
+            if  min_heap:
+                heappop(min_heap)
+                num_events_attended += 1
+                
+        return num_events_attended
