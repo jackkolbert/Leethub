e24e2fe4516601c4e14229fa13a2e74975c87e34
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-07-15 16:57:54 -0400

    Time: 112 ms (77.28%), Space: 20.4 MB (47.69%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,26 +1,9 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        letters_to_words = defaultdict(list)
+        for word in strs:
+            letters_to_words[tuple(sorted(word))].append(word)
+        return list(letters_to_words.values())
         
-        freq_list = []
-        groups = []
-        
-        for string in strs:
-            freq = {}
-            for let in string:
-                if let in freq:
-                    freq[let] += 1
-                else:
-                    freq[let] = 1
-                
-            found = False
-            for i in range(len(freq_list)):
-                if freq == freq_list[i]:
-                    groups[i].append(string)
-                    found = True
-                    break
-            if found is False:  
-                groups.append([string])
-                freq_list.append(freq)
-                    
-        return groups
-                    
