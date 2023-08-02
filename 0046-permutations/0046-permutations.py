a79c67bb8345a9759847d055aa584a0b2d3fd0d7
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2023-08-01 22:06:04 -0400

    Time: 36 ms (99.39%), Space: 16.6 MB (41.62%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        ret = []
+        # c_nums is the rest of the nums, res is current perm
+        def backtrack(c_nums, res):
+            if len(c_nums) == 0:
+                ret.append(res.copy())
+                return
+                
+            for i in range(len(c_nums)):
+                new_list = c_nums[0:i] + c_nums[i+1:len(c_nums)]
+                res.append(c_nums[i])
+                backtrack(new_list, res)
+                res.pop()
+            
+        backtrack(nums, [])
+        return ret
+        
