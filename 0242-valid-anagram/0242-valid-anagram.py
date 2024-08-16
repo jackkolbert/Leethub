92a0b9b7f295bfaab35bd16a46061ca581198a79
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2024-08-16 17:42:48 -0400

    Time: 53 ms (36.04%), Space: 16.8 MB (86.26%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        map1 = {}
+        map2 = {}
+        for letter in s:
+            if letter in map1:
+                map1[letter] += 1
+            else:
+                map1[letter] = 1
+        for letter in t:
+            if letter in map2:
+                map2[letter] += 1
+            else:
+                map2[letter] = 1
+        return map1==map2
