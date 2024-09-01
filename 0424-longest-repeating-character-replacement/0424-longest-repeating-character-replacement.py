13390fc0fd72fcc9f296ce6141f46090c00cdc99
jackkolbert <73853869+jackkolbert@users.noreply.github.com>
2024-09-01 19:36:20 -0400

    Time: 265 ms (7.55%), Space: 16.7 MB (5.64%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,36 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        l = 0
+        r = 0
+        longest = 0
+        window = 26 * [0]
+        window[ord(s[0])-65] += 1
+
+
+        while r < len(s):            
+            # find the current leader
+            leader_letter = None
+            leader_count = 0
+            for i in range(len(window)):
+                if window[i] > leader_count:
+                    leader_count = window[i]
+                    leader_letter = chr(i + 65)
+            print(leader_letter)
+                    
+            window_length = r-l+1
+            if window_length > k + leader_count:
+                window[ord(s[l])-65] -= 1
+                l += 1
+            else:
+                longest = max(longest, window_length)
+                r += 1
+                if r < len(s):
+                    window[ord(s[r])-65] += 1
+        return longest
+
+            
+            
+            
+        
+        
